f4359dad3a93bb88a50902c34f8d39f755a69814
Sugar <73148587+Sugar0612@users.noreply.github.com>
2021-07-25 23:52:01 +0800

    Create hill_sort.cpp

---
+++ Algorithms(算法)/Sorting_algorithm(排序算法)/hill_sort.cpp
@@ -0,0 +1,20 @@
+#pragma once
+
+template<class T>
+
+void hill_sort(T* arr) {
+	int n = arr.size();
+	int gap = n / 2;
+	while (gap > 0) {
+		for (int i = gap; i < n; ++i) {
+			int tmp = arr[i];
+			int j = i - gap;
+			while (j >= 0 && tmp < arr[j]) {
+				arr[j + gap] = arr[j];
+				j -= gap;
+			}
+			arr[j + gap] = tmp;
+		}
+		gap = gap / 2;
+	}
+}
